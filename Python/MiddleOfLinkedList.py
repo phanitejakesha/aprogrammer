bf4321e6f1c55064b8aec33daab4da6d875026f0
phanitejakesha <phani1@umbc.edu>
2018-08-30 21:48:13 -0400

    Changes to MiddleofLinkedList

--- Python/MiddleOfLinkedList.py
+++ Python/MiddleOfLinkedList.py
@@ -1,4 +1,6 @@
-#Given a non-empty, singly linked list with head node head, return a middle node of linked list.If there are two middle nodes, return the second middle node.
+#Given a non-empty, singly linked list with head node head,
+#return a middle node of linked list
+#If there are two middle nodes, return the second middle node.
 
 # Definition for singly-linked list.
 # class ListNode(object):
